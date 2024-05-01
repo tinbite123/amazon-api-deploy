169373bfd863199bcdcf32642f835c380e343afc
TinbiteIsayas <tinbiteisayas@TinbiteayassAir.home>
2024-05-01 19:14:28 +0200

    initial commit for amazon backend deployment

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config()
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const app = express()
+app.use(cors({origin: true}))
+
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.status(200).json({
+        message:"Succses!",
+    });
+});
+
+app.post("/payment/create", async(req, res)=>{
+    const total = req.query.total
+
+    if (total > 0) {
+        const paymentIntent = await stripe.paymentIntent.create({
+            amount: total,
+            currency: "usd"
+        }); 
+        res.status(201).json({
+            clientSecret: paymentIntent.client_secret, 
+        });
+        } else{
+        res.status(403).json({
+            message:("total must be greater than 0 ")
+        });
+    }
+})
+
+
+
+app.listen(4000, (err) => {
+    if (err) throw err;
+    console.log("Amazon Server Running on POST: http://localhost:4000");
+})
